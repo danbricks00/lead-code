992acbdb58cb66236eca29c02ca96c63cd890537
danbricks00 <danbricks@outlook.co.nz>
2025-08-13 13:40:35 +1200

    Fix ERR_HTTP_HEADERS_SENT error in accept-quote.js and decline-quote.js

--- api/accept-quote.js
+++ api/accept-quote.js
@@ -67,7 +67,7 @@ export default async function handler(req, res) {
         details: error.message
       });
     }
+  } else {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
-
-  return res.status(405).json({ error: 'Method not allowed' });
 }
--- api/decline-quote.js
+++ api/decline-quote.js
@@ -69,7 +69,7 @@ export default async function handler(req, res) {
         details: error.message
       });
     }
+  } else {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
-
-  return res.status(405).json({ error: 'Method not allowed' });
 }

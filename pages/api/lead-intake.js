d543554b565f8666a2810dcf214c46bd39754f5d
danbricks00 <danbricks@outlook.co.nz>
2025-09-09 07:48:55 +1200

    typo

--- pages/api/lead-intake.js
+++ pages/api/lead-intake.js
@@ -125,7 +125,7 @@ export default async function handler(req, res) {
     `;
 
     // Send emails
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: process.env.GMAIL_USER,

a00809a9c10ba9127fb5ec93714c524fc54121ea
danbricks00 <danbricks@outlook.co.nz>
2025-09-11 13:50:15 +1200

    syntax

--- pages/api/customer-accept.js
+++ pages/api/customer-accept.js
@@ -19,6 +19,8 @@ const auth = new google.auth.GoogleAuth({
         auth_uri: 'https://accounts.google.com/o/oauth2/auth',
         token_uri: 'https://oauth2.googleapis.com/token',
         auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs'
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
 });
 
 const sheets = google.sheets({ version: 'v4', auth });
